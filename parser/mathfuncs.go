01f562647f2ddfac07d684b01f0b1b5caa7ae6ff
Nv7 <junk4Nv7@gmail.com>
2021-03-31 12:05:27 -0700

    Add some more functions

--- parser/mathfuncs.go
+++ parser/mathfuncs.go
@@ -38,4 +38,70 @@ func mathFuncs() {
 			}, nil
 		}, nil
 	}
+
+	funcs["CEIL"] = func(args []string, line int) (Executable, error) {
+		if len(args) != 1 {
+			return nil, fmt.Errorf("line %d: invalid argument amount for function %s", line, "FLOOR")
+		}
+		ex1, err := parseStmt(args[0], line)
+		if err != nil {
+			return nil, err
+		}
+		return func(p *Program) (Variable, error) {
+			val, err := ex1(p)
+			if err != nil {
+				return Variable{}, err
+			}
+			if val.Type != FLOAT && val.Type != INT {
+				return Variable{}, fmt.Errorf("line %d: input for FLOOR must be float or integer", line)
+			}
+			if val.Type.IsEqual(INT) {
+				val.Data = float64(val.Data.(int))
+			}
+			num := math.Ceil(val.Data.(float64))
+			if val.Type.IsEqual(FLOAT) {
+				return Variable{
+					Type: FLOAT,
+					Data: num,
+				}, nil
+			}
+			return Variable{
+				Type: INT,
+				Data: int(num),
+			}, nil
+		}, nil
+	}
+
+	funcs["FLOOR"] = func(args []string, line int) (Executable, error) {
+		if len(args) != 1 {
+			return nil, fmt.Errorf("line %d: invalid argument amount for function %s", line, "FLOOR")
+		}
+		ex1, err := parseStmt(args[0], line)
+		if err != nil {
+			return nil, err
+		}
+		return func(p *Program) (Variable, error) {
+			val, err := ex1(p)
+			if err != nil {
+				return Variable{}, err
+			}
+			if val.Type != FLOAT && val.Type != INT {
+				return Variable{}, fmt.Errorf("line %d: input for FLOOR must be float or integer", line)
+			}
+			if val.Type.IsEqual(INT) {
+				val.Data = float64(val.Data.(int))
+			}
+			num := math.Round(val.Data.(float64))
+			if val.Type.IsEqual(FLOAT) {
+				return Variable{
+					Type: FLOAT,
+					Data: num,
+				}, nil
+			}
+			return Variable{
+				Type: INT,
+				Data: int(num),
+			}, nil
+		}, nil
+	}
 }
