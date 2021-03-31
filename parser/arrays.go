2564bee3693a173963559fe89b10e055c12ce7aa
Nv7 <junk4Nv7@gmail.com>
2021-03-31 13:18:32 -0700

    REPEAT support

--- parser/arrays.go
+++ parser/arrays.go
@@ -71,4 +71,59 @@ func arrayFuncs() {
 			return arr.Data.([]Variable)[index.Data.(int)], nil
 		}, nil
 	}
+
+	funcs["REPEAT"] = func(args []string, line int) (Executable, error) {
+		if len(args) != 2 {
+			return nil, fmt.Errorf("line %d: invalid argument amount for function %s", line, "INDEX")
+		}
+		ex1, err := parseStmt(args[0], line)
+		if err != nil {
+			return nil, err
+		}
+		ex2, err := parseStmt(args[1], line)
+		if err != nil {
+			return nil, err
+		}
+		return func(p *Program) (Variable, error) {
+			arr, err := ex1(p)
+			if err != nil {
+				return Variable{}, err
+			}
+			if !arr.Type.IsEqual(ARRAY) && !arr.Type.IsEqual(STRING) {
+				return Variable{}, fmt.Errorf("line %d: parameter 1 of INDEX must be array or string", line)
+			}
+			cnt, err := ex2(p)
+			if err != nil {
+				return Variable{}, err
+			}
+			if !cnt.Type.IsEqual(INT) {
+				if cnt.Type.IsEqual(FLOAT) {
+					cnt.Type = INT
+					cnt.Data = int(cnt.Data.(float64))
+				} else {
+					return Variable{}, fmt.Errorf("line %d: parameter 2 of INDEX must be integer", line)
+				}
+			}
+			if arr.Type.IsEqual(STRING) {
+				out := ""
+				for i := 0; i < cnt.Data.(int); i++ {
+					out += arr.Data.(string)
+				}
+				return Variable{
+					Type: STRING,
+					Data: out,
+				}, nil
+			}
+			out := make([]Variable, len(arr.Data.([]Variable))*cnt.Data.(int))
+			for i := 0; i < len(out); i += len(arr.Data.([]Variable)) {
+				for j := 0; j < len(arr.Data.([]Variable)); j++ {
+					out[i+j] = arr.Data.([]Variable)[j]
+				}
+			}
+			return Variable{
+				Type: ARRAY,
+				Data: out,
+			}, nil
+		}, nil
+	}
 }
