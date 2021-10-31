b7f1133000fdc19cdbc21dda88ed16ff25814fb4
Nv7 <junk4Nv7@gmail.com>
2021-10-31 12:23:30 -0700

    Fix parser

--- parser/parse.go
+++ parser/parse.go
@@ -9,35 +9,39 @@ func ParseCode(code string, pos *Pos) ([]Statement, error) {
 	if len(code) == 0 {
 		return make([]Statement, 0), nil
 	}
-	openBrackets := 0
-	openQuotation := false
-	stmts := make([]Statement, 0)
 
+	inFnName := true
 	fnName := ""
 	arg := ""
 	args := make([]string, 0)
-	inFnName := true
+
+	openBrackets := 0
+	openQuotation := false
+
+	stmts := make([]Statement, 0)
 
 	for _, char := range code {
 		switch char {
 		case '[':
-			if openBrackets == 0 {
-				inFnName = true
-			}
 			openBrackets++
+			if !inFnName {
+				arg += string(char)
+			}
+
 		case ']':
 			openBrackets--
 
-			if openBrackets == 0 && !openQuotation && fnName != "" {
+			if !openQuotation && openBrackets == 0 {
+				// Add last arg
 				args = append(args, arg)
+
 				argVals := make([]Statement, 0)
-				for _, v := range args {
-					v = strings.TrimSpace(v)
-					parsed, err := ParseCode(v, pos.Duplicate())
+				for _, arg := range args {
+					argV, err := ParseCode(arg, pos)
 					if err != nil {
 						return nil, err
 					}
-					argVals = append(argVals, parsed...)
+					argVals = append(argVals, argV...)
 				}
 				stmt, err := GetStatement(fnName, argVals)
 				if err != nil {
@@ -45,47 +49,55 @@ func ParseCode(code string, pos *Pos) ([]Statement, error) {
 				}
 				stmts = append(stmts, stmt)
 
+				inFnName = true
 				fnName = ""
 				arg = ""
 				args = make([]string, 0)
-
-				continue
+			} else if inFnName {
+				fnName += string(char)
+			} else {
+				arg += string(char)
 			}
+
 		case '"':
 			openQuotation = !openQuotation
+			if inFnName {
+				fnName += string(char)
+			} else {
+				arg += string(char)
+			}
+
 		case '\n', ' ':
 			if char == '\n' {
 				pos.NextLine()
 			}
 
-			if openBrackets == 1 && !openQuotation {
+			if !openQuotation && openBrackets == 1 {
 				if inFnName {
-					// [DEFINE a 0]
-					//        ^
-					fnName = strings.TrimSpace(fnName)
-					fnName = fnName[1:]
 					inFnName = false
 				} else {
-					// [DEFINE a 0]
-					//          ^
 					args = append(args, arg)
 					arg = ""
 				}
-
-				continue
+			} else {
+				if inFnName {
+					fnName += string(char)
+				} else {
+					arg += string(char)
+				}
 			}
-		}
 
-		if inFnName {
-			fnName += string(char)
-		} else {
-			arg += string(char)
+		default:
+			if inFnName {
+				fnName += string(char)
+			} else {
+				arg += string(char)
+			}
 		}
 	}
 
 	if len(stmts) == 0 {
 		return []Statement{GetConst(code)}, nil
 	}
-
 	return stmts, nil
 }
